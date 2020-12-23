85785cadc3b811c2dfba13f1930620a91713d4e9
Farshad Shafii <farshad.shafii@gmail.com>
2020-12-23 23:29:42 +0000

    added drop down box toggle

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 let endpoint =
   "https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID={API_KEY}";
 
+let dropdown = document.getElementById("dropdown");
+let dropdownBox = document.getElementById("dropdown-box");
+
+dropdown.addEventListener("click", function () {
+  dropdownBox.style.display !== "none"
+    ? (dropdownBox.style.display = "none")
+    : (dropdownBox.style.display = "block");
+});
+
 fetch(endpoint)
   .then((response) => response.json())
   .then((data) => {})
