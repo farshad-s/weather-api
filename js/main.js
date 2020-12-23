1f8cf485224fcecbcba9da5ad45062cc7372c2a4
Farshad Shafii <farshad.shafii@gmail.com>
2020-12-23 22:19:28 +0000

    added endpoint + removed api key

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
-fetch()
+let endpoint =
+  "https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID={API_KEY}";
+
+fetch(endpoint)
   .then((response) => response.json())
   .then((data) => {})
   .catch((error) => {
