335a7bdab32f251d3d293fbd7078b5635dac87a2
Farshad Shafii <farshad.shafii@gmail.com>
2020-12-26 15:11:08 +0000

    removed top card + added search bar

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@ let endpoint =
 
 let dropdown = document.getElementById("dropdown");
 let dropdownBox = document.getElementById("dropdown-box");
-let cardRoot = document.getElementById("card-root");
 let dropdownRoot = document.getElementById("dropdown-root");
+let searchbar = document.getElementById("city-search");
 
 dropdown.addEventListener("click", function () {
   dropdownBox.style.display !== "block"
@@ -20,17 +20,6 @@ fetch(endpoint)
       weatherDescription = data.weather[0].description,
       minTemp = data.main.temp_min,
       maxTemp = data.main.temp_max;
-
-    cardRoot.innerHTML = `<div class="card-image">
-      <img
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Unusually_well_defined_warm_front.jpg/200px-Unusually_well_defined_warm_front.jpg"
-      />
-      <span class="card-title" id="card-title">${name}</span>
-    </div>
-    <div class="card-content">
-      <p id="card-text-type">Weather: ${currentWeather}</p>
-      <p id="card-text-temp">Description: ${weatherDescription}</p>
-    </div>`;
 
     dropdownRoot.innerHTML = `<ul>
     <li class="dropdown-header">
